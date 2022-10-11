2c50d3bb04b289e29faa445f48898f244bb285f3
rlopes1 <44005400+rlopes1@users.noreply.github.com>
2022-10-11 10:04:04 -0400

    Mostly finished visual revamp.
    Style of website is mostly done. Added imgs and and img carousel on the popups of project section and fixed most weird behavior. Need to update resume files, fix carousel on first page load, and fix overall responsiveness.

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,57 +1,72 @@
 /*const text = document.querySelector(".introtext");
 const strText = text.textContent;
 
-const splitText = strText.split("");
+const splitText = strText.split(" ");
 
 
 text.textContent = "";
 
-//creating an array to keep track of space in the paragraph
-let sparray = new Array();
-for(let i = 0; i < splitText.length; i++) {
-   if(strText.charAt(i) == ' ') {
-      sparray[i] = 1;   //1 for space
-   } else {
-      sparray[i] = 0;   //0 for other
-   }
-}
-
 //creating a character array for our spans
 for(let i=0; i < splitText.length; i++) {
-   if(sparray[i] == 1) {
       text.innerHTML += "<span>" + splitText[i] + "</span>";
       text.innerHTML += " ";
-   } else {
-      text.innerHTML += "<span>" + splitText[i] + "</span>";
-   }
 }
 
+console.log(splitText)
 
-let char = 0;
+let word = 0;
 let timer = setInterval(onTick, 60);
 
 function onTick() {
-   const span = text.querySelectorAll('span')[char];
+   const span = text.querySelectorAll('span')[word];
    span.classList.add('fade');
-   char++;
-   if(char === splitText.length) {
+   word++;
+   if(word === splitText.length) {
       complete();
       return;
    }
 }
-
 function complete() {
    clearInterval(timer);
    timer = null;
-}
-*/
+}*/
 
 
 let popupArray = document.querySelectorAll('.popup');
-
 function openPopup(i) {
    popupArray[i].classList.add("openPopup");
 }
 function closePopup(i) {
    popupArray[i].classList.remove("openPopup");
 }
+
+
+
+const slides = document.querySelectorAll(".slide");
+let currSlide = 0;
+
+const nextSlide = document.querySelector(".nextbutton");
+let maxSlides = slides.length - 1;
+nextSlide.addEventListener("click", function () {
+   if(currSlide == maxSlides) {
+      currSlide = 0;
+   } else {
+   currSlide++;
+   }
+   slides.forEach((slide, idx) => {
+      slide.style.transform = `translateX(${(idx - currSlide)* 100}%)`;
+   });
+});
+
+const prevSlide = document.querySelector(".prevbutton");
+prevSlide.addEventListener("click", function(){
+   if(currSlide == 0) {
+      currSlide = maxSlides;
+   } else {
+      currSlide--;
+   }
+   slides.forEach((slide, idx) => {
+      slide.style.transform = `translateX(${(idx - currSlide)* 100}%)`;
+   });
+})
+
